02277feb08a65810411e6e1a0565b85070a1fa76
gilMars <gilmar.lilium@gmail.com>
2018-08-15 15:39:01 -0300

    Ajustado diretório de urls da aplicação

--- controlpanel/urls.py
+++ controlpanel/urls.py
@@ -1,8 +1,19 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
-
+from django.contrib.auth import views as auth_views
 urlpatterns = [
-    #path('',views.loginAuth, name='loginAuth'),
-    path('cadastro/', views.registerUser, name='registerUser'),
+
+
     path('painel/', views.painelUser, name='painelUser'),
+
+    # URL para página de login costumizada
+    path('', auth_views.login, {'template_name': 'controlpanel/login.html'}),
+
+    # URL para logout
+    path('logout/', auth_views.logout, {'next_page': '/'}),
+    
+    # Autenticação por rede social
+    path('oauth/', include('social_django.urls', namespace='social')),
+
+    path('cadastro/', views.cadastro)
 ]
