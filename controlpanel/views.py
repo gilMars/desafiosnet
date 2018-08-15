d2b6106d4fa89a9af637f18865f8eb12f8c61a87
gilMars <gilmar.lilium@gmail.com>
2018-08-15 15:40:39 -0300

    Adicionado suporte para cadastro de usuários

--- controlpanel/views.py
+++ controlpanel/views.py
@@ -1,13 +1,34 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
-def loginAuth(request):
-    return render(request, 'controlpanel/login.html', {})
 
-def registerUser(request):
-    return render(request, 'controlpanel/cadastro.html', {})
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect
+from django import forms
+from .forms import UserRegistrationForm
 
 @login_required(login_url='/')
 def painelUser(request):
     return render(request, 'controlpanel/painel.html', {})
 # Create your views here.
+
+
+def cadastro(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            userObj = form.cleaned_data
+            username = userObj['username']
+            email =  userObj['email']
+            password =  userObj['password']
+            if not (User.objects.filter(username=username).exists() or User.objects.filter(email=email).exists()):
+                User.objects.create_user(username, email, password)
+                user = authenticate(username = username, password = password)
+                login(request, user)
+                return HttpResponseRedirect('/')
+            else:
+                raise forms.ValidationError('Looks like a username with that email or password already exists')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'controlpanel/cadastro.html', {'form' : form})
