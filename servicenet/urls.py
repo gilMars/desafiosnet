395b7674e937069c0024f584b4a1d9444b130b54
gilMars <gilmar.lilium@gmail.com>
2018-08-15 02:20:18 -0300

    Adicionado sistema de login

--- servicenet/urls.py
+++ servicenet/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', auth_views.login, {'template_name': 'controlpanel/login.html'}),
+    path('logout/', auth_views.logout),
     path('',include('controlpanel.urls')),
 ]
