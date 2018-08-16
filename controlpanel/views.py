5aa568a7a99ad062069d0755babe08d6627f2f3c
gilMars <gilmar.lilium@gmail.com>
2018-08-16 02:33:46 -0300

    Adicionado funcionalidade de cadastrar cliente

--- controlpanel/views.py
+++ controlpanel/views.py
@@ -9,19 +9,45 @@ from django.contrib.auth import authenticate, login
 from django.http import HttpResponseRedirect
 from django import forms
 from .forms import UserRegistrationForm
-
-
+from .forms import CadastroClienteForm
 from .models import Cliente
 from .models import Endereco
 
+
 @login_required(login_url='/')
 def painelUser(request):
-    clientes = Cliente.objects.all()
-    return render(request, 'controlpanel/painel.html', {'clientes':clientes})
-# Create your views here.
+    clientes = Cliente.objects.filter(user_id=request.user)
+    return render(request, 'controlpanel/painel.html', {'clientes': clientes})
 
+
+@login_required(login_url='/')
 def cadastrar_cliente(request):
-    return render(request, 'controlpanel/cadastrar.html')
+    if request.method == 'POST':
+        form = CadastroClienteForm(request.POST)
+       # validForm = form.is_valid()
+       # if validForm:
+        form.is_valid()
+        clienteObj = form.cleaned_data
+        nome = clienteObj['nome']
+        telefone = clienteObj['telefone']
+        cep = clienteObj['cep']
+        endereco = clienteObj['endereco']
+        numero = clienteObj['numero']
+        cidade = clienteObj['cidade']
+        estado = clienteObj['estado']
+        pais = clienteObj['pais']
+        # clienteExists = Cliente.objects.filter(user_id=request.user,nome=nome).exists()
+        # if userExists:
+        #form.add_error(None, "Cliente já está cadastrado")
+        # validForm = form.is_valid()
+        # if validForm:
+        cliente = Cliente(user_id=request.user, nome=nome, telefone=telefone)
+        cliente.save()
+        Endereco.objects.create(cliente_id=cliente, cep=cep, endereco=endereco, cidade=cidade, numero=numero, estado=estado, pais=pais)
+    else:
+        form = CadastroClienteForm()
+    return render(request, 'controlpanel/cadastrar.html', {'form': form})
+
 
 def registrar(request):
     if request.method == 'POST':
@@ -30,8 +56,8 @@ def registrar(request):
         if validForm:
             userObj = form.cleaned_data
             username = userObj['username']
-            email =  userObj['email']
-            password =  userObj['password']
+            email = userObj['email']
+            password = userObj['password']
             userExists = User.objects.filter(username=username).exists()
             emailExists = User.objects.filter(email=email).exists()
             if userExists:
@@ -41,12 +67,9 @@ def registrar(request):
             validForm = form.is_valid()
             if validForm:
                 User.objects.create_user(username, email, password)
-                user = authenticate(username = username, password = password)
+                user = authenticate(username=username, password=password)
                 login(request, user)
                 return HttpResponseRedirect('/')
     else:
         form = UserRegistrationForm()
-    return render(request, 'controlpanel/cadastro.html', {'form' : form})
-
-#def listar_clientes(request):
-
+    return render(request, 'controlpanel/cadastro.html', {'form': form})
