1b0941ea7f2d4f0067ac2823b0cb386496321a31
gilMars <gilmar.lilium@gmail.com>
2018-08-15 21:31:45 -0300

    Correção

--- controlpanel/urls.py
+++ controlpanel/urls.py
@@ -7,13 +7,13 @@ urlpatterns = [
     path('painel/', views.painelUser, name='painelUser'),
 
     # URL para página de login costumizada
-    path('', auth_views.login, {'template_name': 'controlpanel/login.html'}),
+    path('', auth_views.login, {'template_name': 'controlpanel/login.html'}, name='login'),
 
     # URL para logout
-    path('logout/', auth_views.logout, {'next_page': '/'}),
+    path('logout/', auth_views.logout, {'next_page': '/'}, name='logout'),
     
     # Autenticação por rede social
     path('oauth/', include('social_django.urls', namespace='social')),
 
-    path('cadastro/', views.cadastro)
+    path('cadastro/', views.cadastro, name='cadastro')
 ]
